9b4e6cb4f17fc5e2207d1149c449ee55fcc48aa9
PedroFolsterSanches <161752020+PedroFolsterSanches@users.noreply.github.com>
2024-04-01 19:36:40 -0300

    fazendo o shop

--- projeto-borbalandia/test-pag-1/scripts/main.js
+++ projeto-borbalandia/test-pag-1/scripts/main.js
@@ -1,71 +1,82 @@
-    //#region script clicker
+let apc = 1;
+let aps = 0;
+let aboboras = 0;
+const image = document.querySelector('#imagem');
+const textoAbobora = document.querySelector('#moedas1-count');
+const textoAps = document.querySelector('#moedas2-count');
+const formatLetters = ["", "k", "M", "B", "T", "q", "Q", "s", "S", "O", "N", "d", "U", "D", "Tr", "Qu", "QU", "Se", "SE", "Oc", "No", "V"];
 
-    let apc = 1;
-    let aps = 100;
-    let aboboras = 0;
-    const image = document.querySelector('#imagem');
-    const textoAbobora = document.querySelector('#moedas1-count');
-    const textoAps = document.querySelector('#moedas2-count');
-    const formatLetters = ["", "k", "M", "B", "T", "q", "Q", "s", "S", "O", "N", "d", "U", "D", "Tr", "Qu", "QU", "Se", "SE", "Oc", "No", "V"];
-
-    function formatNumber(number) {
-        const index = Math.floor(Math.log10(aboboras)/3)
-        if (number >= 1000) {
-            return (number/Math.pow(10, (Math.floor(Math.log10(aboboras)/3))*3)).toFixed(2) + formatLetters[index];
-        } else return number.toFixed(0);
-    }
-
-    /*function autoIncrement() {
-        aboboras += aps/100;
-        textoAbobora.textContent = formatNumber(aboboras);
-        textoAps.textContent = formatNumber(aps);
-    }
-    setInterval(autoIncrement, 10);*/
-
-    // adiciona um evento para os cliques na imagem
-    image.addEventListener('click', () => {
-        let timeoutHandle;
-        let podeclicar = true;
-
-        if (!podeclicar) {
-            return;
-        }
-        if (timeoutHandle) {
-            clearTimeout(timeoutHandle);
-        }
-
-        // pega o valor de moedas1
-        aboboras = parseInt(textoAbobora.textContent);
-        // aumenta o valor em 1
-        aboboras += apc;
-
-        // atualiza o valor
-        textoAbobora.textContent = formatNumber(aboboras);
-
-        // Set up a delay to allow a 0.05-second window for another click event
-        podeclicar = false;
-        timeoutHandle = setTimeout(() => {
-            podeclicar = true;
-            timeoutHandle = null;
-        }, 50);
-    });
-
-    //#endregion script clicker
-
-    //#region script menu
-
-function toggleMenu(targetId) {
-    // seleciona a section da tela principal
-    let mainScreen = document.querySelectorAll('.conteudo-tela');
-
-    // muda o display dos elementos
-    mainScreen.forEach((item) => {
-        if (item.id === targetId) {
-            item.style.display = 'block';
-        } else {
-            item.style.display = 'none';
-        }
-    });
+function formatNumber(number) {
+    const index = Math.floor(Math.log10(aboboras)/3)
+    if (number >= 1000) {
+        return (number/Math.pow(10, (Math.floor(Math.log10(aboboras)/3))*3)).toFixed(2) + formatLetters[index];
+    } else return number.toFixed(0);
 }
 
-    //#endregion script menu
+//#region script clicker
+
+// adiciona um evento para os cliques na imagem
+image.addEventListener('click', () => {
+    let timeoutHandle;
+    let podeclicar = true;
+
+    if (!podeclicar) {
+        return;
+    }
+    if (timeoutHandle) {
+        clearTimeout(timeoutHandle);
+    }
+
+    // pega o valor de moedas1
+    aboboras = parseInt(textoAbobora.textContent);
+    // aumenta o valor em 1
+    aboboras += apc;
+
+    // atualiza o valor
+    textoAbobora.textContent = formatNumber(aboboras);
+
+    // ativa um delay de 0,05 segundos
+    podeclicar = false;
+    timeoutHandle = setTimeout(() => {
+        podeclicar = true;
+        timeoutHandle = null;
+    }, 50);
+});
+
+//#endregion script clicker
+
+//#region loja
+
+const botaoCompraFilho = document.querySelector('#botao-compra-filho');
+
+botaoCompraFilho.addEventListener('click', () => {
+    aboboras -= 1;
+    aps += 0.7;
+    textoAps.textContent = formatNumber(aps);
+});
+
+//#endregion loja
+
+//#region script menu
+
+function toggleMenu(targetId) {
+// seleciona a section da tela principal
+let mainScreen = document.querySelectorAll('.conteudo-tela');
+
+// muda o display dos elementos
+mainScreen.forEach((item) => {
+    if (item.id === targetId) {
+        item.style.display = 'block';
+    } else {
+        item.style.display = 'none';
+    }
+});
+}
+//#endregion script menu
+
+function autoIncrement() {
+    aboboras += aps/100;
+    textoAbobora.textContent = formatNumber(aboboras);
+    textoAps.textContent = formatNumber(aps);
+}
+setInterval(autoIncrement, 10);
